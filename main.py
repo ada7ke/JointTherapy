55dfd8d70e399729bf1ce8876822c4f1048ac980
Ada <adayke10@gmail.com>
2025-10-09 11:57:22 -0700

    video + color mask

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-import cv2
+import cv2, eyw
 
 camera_feed = cv2.VideoCapture(0)
-ret, frame = camera_feed.read()
-
 cv2.namedWindow("Camera Feed")
-cv2.imshow("Camera Feed", frame)
+cv2.namedWindow("Mask")
 
-keypressed = cv2.waitKey(0)
-if keypressed == 27:
-    camera_feed.release()
-    cv2.destroyAllWindows()
+while True:
+    ret, frame = camera_feed.read()
+    cv2.imshow("Camera Feed", frame)
+
+    mask = eyw.create_mask(frame, [150, 150, 0], [255, 255, 255])
+    masked_image = eyw.apply_mask(frame, mask)
+    cv2.imshow("Mask", masked_image)
+
+    keypressed = cv2.waitKey(1)
+    if keypressed == 27:
+        break
+
+camera_feed.release()
+cv2.destroyAllWindows()
